3fa3a8876dd3c5507a5229cccfdb4c0cb112a8f7
kimminjae <96830595+kareawe@users.noreply.github.com>
2024-04-04 13:12:10 +0900

    Update member6.jsx

--- src/pages/memberInfo/member6.jsx
+++ src/pages/memberInfo/member6.jsx
@@ -1,47 +1,190 @@
+// eslint-disable-next-line
+
 import React from "react";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Col, Container, ListGroup } from "react-bootstrap";
+import { FiCheckCircle } from "react-icons/fi";
 
-const MemberDiv = styled.div`
-  margin-top: 40px;
+import "./css/Member.css";
+import "./css/Profile.css";
+const StyledLink = styled(Link)`
+  width: calc(33.333% - 20px);
+  text-decoration: none;
+  color: inherit;
 
-  p {
-    font-size: 14px;
-    font-weight: 600;
+  img {
+    width: 300px;
+    height: auto;
   }
-`;
 
-const ImagesDiv = styled.div`
-  display: flex;
-  margin-bottom: 40px;
-`;
-
-const IntroductionDiv = styled.div`
   p {
-    font-size: 20px;
+    padding-left: 80px;
   }
 `;
 
 function Member6() {
   return (
-    <MemberDiv>
-      <ImagesDiv>
+    <div className="col-sm-12 col-md-4">
+      <div className="item">
         <img
-          src=""
-          style={{ width: "400px", height: "400px", paddingRight: "100px" }}
-        ></img>
-        <img
-          src=""
-          alt="image"
-          style={{ width: "55vw", height: "400px" }}
-        ></img>
-      </ImagesDiv>
-      <IntroductionDiv>
-        <p>Name: </p>
-        <p>Age: </p>
-        <p>Address: </p>
-        <p>What I want to say: </p>
-      </IntroductionDiv>
-    </MemberDiv>
+          style={{ borderRadius: "50%", width: "150px", height: "150px" }}
+          src="https://avatars.githubusercontent.com/u/96830595?s=400&v=4"
+          alt="Yoon Sang"
+        />
+
+        <Card
+          style={{
+            borderRadius: "12px",
+            marginBottom: "24px",
+            textAlign: "center",
+          }}
+        >
+          <Card.Body style={{ padding: "12px", backdropFilter: "blur(10px)" }}>
+            {/* 이곳에 본인의 간단한 소개말 적어주세요 */}
+            <p>Hello!! I'm Kim Min Jae.</p>
+            <p>Student of Gachon Univ finance mathematics Dept.</p>
+            <p>Working hard to be a frontend developer.</p>
+          </Card.Body>
+        </Card>
+
+        {/* 이곳에 본인 이름과 학교 적어주세요 */}
+        <h3>Kim Min Jae</h3>
+        <div className="info">
+          <p>Student of Gachon Univ finance mathematics Dept</p>
+        </div>
+        <div>
+          <h2>Tech Stack</h2>
+          {/* 본인 tech stack 이미지를 적용시켜주세요 */}
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png"
+              style={{ width: "50px", height: "50px", marginRight: "10px" }}
+            />
+            <img
+              src={require("./images/yoonsang_skills/javascript.png")}
+              style={{ width: "50px", height: "50px", marginRight: "10px" }}
+            />
+            <img
+              src={require("./images/yoonsang_skills/python.png")}
+              style={{ width: "50px", height: "50px", marginRight: "10px" }}
+            />
+            <img
+              src={require("./images/yoonsang_skills/react.png")}
+              style={{ width: "50px", height: "50px", marginRight: "10px" }}
+            />
+            <img
+              src="https://img1.daumcdn.net/thumb/R750x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FH9zLv%2FbtrjmWMOHFX%2F7Ko3OJ1Vc1fmgUw9PVZU7K%2Fimg.png"
+              style={{ width: "50px", height: "50px" }}
+            />
+          </div>
+        </div>
+        <h3>Yoon Sang On The Web Site</h3>
+        <ul>
+          {/* 본인 관련 웹사이트 링크와 이미지 적용시켜 주세요 */}
+          <StyledLink href="https://github.com/kareawe">
+            <span style={{ marginRight: "20px" }}>
+              <img
+                src={require("./images/yoonsang_web_icon/github.png")}
+                alt="GitHub Icon"
+                style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+              />
+            </span>
+          </StyledLink>
+          <StyledLink href="https://blog.naver.com/kimminjae1573">
+            <span style={{ marginRight: "20px" }}>
+              <img
+                src={require("./images/yoonsang_web_icon/naverBlog.jpg")}
+                alt="Naver Blog Icon"
+                style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+              />
+            </span>
+          </StyledLink>
+        </ul>
+        <Container>
+          {/* 본인의 올해 목표 적어주세요 */}
+          <h3>Goals In 2024</h3>
+          <ListGroup>
+            <ListGroup.Item className="d-flex align-items-center">
+              <FiCheckCircle className="mr-3" />
+              SQLD 자격증
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex align-items-center">
+              <FiCheckCircle className="mr-3" />
+              대외활동 (우체국 예금 서포터즈)
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex align-items-center">
+              ADSP 자격증
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex align-items-center">
+              정보처리기사
+            </ListGroup.Item>
+          </ListGroup>
+
+          {/* 본인의 취미를 적어주세요 */}
+          <div
+            className="styledList"
+            style={{
+              listStyleType: "none",
+              textAlign: "left",
+              paddingLeft: "0",
+              marginTop: "50px",
+            }}
+          >
+            <ul>
+              <h3>Favorites</h3>
+              <li>Football</li>
+              <li>Game</li>
+              <li>weight training</li>
+              <li>movie</li>
+            </ul>
+          </div>
+
+          {/* 본인 프로필 적어주세요 */}
+          <div
+            className="styledList"
+            style={{
+              listStyleType: "none",
+              textAlign: "left",
+              paddingLeft: "0",
+              marginTop: "50px",
+            }}
+          >
+            <ul>
+              <h3>Profile</h3>
+              <li>Born in: 2001</li>
+              <li>Middle School in: 2014 ~ 2016</li>
+              <li>High School in: 2017 ~ 2019</li>
+              <li>Gachon Univ in: 2020 ~ </li>
+              <li>Army in: 2021 ~ 2022</li>
+            </ul>
+          </div>
+        </Container>
+
+        <div id="works" style={{ marginTop: "100px" }}>
+          <h3>My Projects</h3>
+
+          {/* 본인 프로젝트 소갯말과 github링크 적용시켜 주세요 */}
+          <ul>
+            <div style={{ marginTop: "20px" }}>
+              <h3>Mingle:당근마켓 clone coding</h3>
+              <p>Working Period: 2021.11 ~ 2022.02</p>
+              <p>Role: front-end</p>
+              <li>
+                <div>
+                  <img
+                    src="https://velog.velcdn.com/images/ohmynoon__/post/acc58798-6a23-42d1-a9bf-bda28dfc82f7/image.jpg"
+                    alt="mingle"
+                    style={{ width: "400px", height: "300px" }}
+                  />
+                </div>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
